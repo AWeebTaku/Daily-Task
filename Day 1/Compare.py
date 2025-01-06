fd97750beefd45c5e4ed7d0b31f78bf5c8496f58
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-06 16:47:56 +0530

    Create Compare.py

---
+++ Day 1/Compare.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter a number:"))
+if num1 < num2:
+    print(num2 ,"is Greater than", num1)
+else:
+    print(num1 ,"is Greater than", num2)
