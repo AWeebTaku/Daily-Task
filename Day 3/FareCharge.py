365ce5c4ec957b28cb73c18ecc6632a051ce75f3
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 18:11:07 +0530

    Update FareCharge.py

--- Day 3/FareCharge.py
+++ Day 3/FareCharge.py
@@ -4,3 +4,19 @@
 # 		51-100						10 Rs/Km
 # 		>100						12 Rs/Km
 
+Distance = int(input("Enter Distance in Kilometers: "))
+
+if Distance >= 1 and Distance <= 50:
+	Charges = 8
+	Fare = Charges*Distance
+	print("Total Fare:",Fare)
+elif Distance >= 51 and Distance <= 100:
+	Charges = 10
+	Fare = Charges*Distance
+	print("Total Fare:",Fare)
+elif Distance>100:
+	Charges = 12
+	Fare = Charges*Distance
+	print("Total Fare:",Fare)
+else:
+	print("Invalid Journey Distance")
