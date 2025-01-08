9a436880d6a952a495c82f9fde8ccab332f2a2ee
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 16:13:44 +0530

    Update EvenOdd.py

--- Day 2/EvenOdd.py
+++ Day 2/EvenOdd.py
@@ -1,4 +1,5 @@
 #1. Using input() function take one number from the user and using ternary operators check whether the number is even or odd
+
 num1 = int(input("Enter a number: "))
 if num1 == 0:
 	print("Number is neither even nor odd")
