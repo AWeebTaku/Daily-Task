c3c94001a20aad55f6d2b9986c51abedbac0ce36
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 16:56:29 +0530

    Update SwapNum.py

--- Day 2/SwapNum.py
+++ Day 2/SwapNum.py
@@ -2,12 +2,14 @@
 
 num1 = int(input("Enter number One:"))
 num2 = int(input("Enter number Two:"))
+temp1 = 0
 
 print("Number One is:",num1)
 print("Number Two is:",num2)
 
+temp1 = num1
 num1 = num2
 print("Number One is:",num1)
 
-num2 = num1
-print("Number Two is:",num2)
+num2 = temp1
+print("Number Two is:",temp1)
