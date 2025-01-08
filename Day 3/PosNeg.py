46db53067bbc8cce2aa274faa86344ee7b04071b
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 17:31:26 +0530

    Update PosNeg.py

--- Day 3/PosNeg.py
+++ Day 3/PosNeg.py
@@ -1,2 +1,12 @@
 #3. Python Program to Check if a Number is Positive, Negative or 0
 
+number = int(input("Enter a Number: "))
+
+if number == 0:
+	print("Number is Zero")
+elif number>0:
+	print("Number is Positive")
+elif number<0:
+	print("Number is Negative")
+else:
+	print("Invalid Number")
