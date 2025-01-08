7a09057525ea58ac42bcf4555a14d6d2dab75e75
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 16:13:03 +0530

    Update AreaTri.py

--- Day 1/AreaTri.py
+++ Day 1/AreaTri.py
@@ -1,3 +1,5 @@
+#4. Python program to find the area of a triangle whose sides are given
+
 triB = int(input("Enter Breath of Triangle:"))
 triH = int(input("Enter Height of Triangle:"))
 areatri = 1/2*(triB*triH)
