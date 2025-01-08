ff0fc0423d39339cecf61cb1f13cb839d6e13fed
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 16:51:48 +0530

    Create SwapNum.py

---
+++ Day 2/SwapNum.py
@@ -0,0 +1,13 @@
+#2. Using input function take two number and then swap the number
+
+num1 = int(input("Enter number One:"))
+num2 = int(input("Enter number Two:"))
+
+print("Number One is:",num1)
+print("Number Two is:",num2)
+
+num1 = num2
+print("Number One is:",num1)
+
+num2 = num1
+print("Number Two is:",num2)
