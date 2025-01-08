83f4d332a47942111dc1c4d9f66c77adfffdee28
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 17:16:40 +0530

    Update AreaTri.py

--- Day 1/AreaTri.py
+++ Day 1/AreaTri.py
@@ -1,6 +1,6 @@
 #4. Python program to find the area of a triangle whose sides are given
 
-Breath = int(input("Enter Breath of Triangle:"))
-Height = int(input("Enter Height of Triangle:"))
+Breath = int(input("Enter Breath of Triangle: "))
+Height = int(input("Enter Height of Triangle: "))
 Area = 1/2*(Breath*Height)
-print("Area of Triangle = ",Area)
+print("Area of Triangle =",Area)
