7bd8744f7de3b8859c20bc4ceeb145d4b0650470
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 17:22:03 +0530

    Update FindLarge.py

--- Day 3/FindLarge.py
+++ Day 3/FindLarge.py
@@ -1,3 +1,5 @@
+#2. Python Program to Find the Largest Among Three Numbers
+
 num1 = int(input("Enter number one: "))
 num2 = int(input("Enter number two: "))
 num3 = int(input("Enter number three: "))
