bffda5f0950405c8e1061f8aa5a26862699cfc9b
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 17:22:07 +0530

    Update ProductAmount.py

---
+++ Day 3/ProductAmount.py
@@ -0,0 +1,7 @@
+# 4. A toy vendor supplies three types of toys: Battery Based Toys, Key-based Toys, and Electrical Charging Based Toys.
+# The vendor gives a discount of 10% on orders for battery-based toys if the order is for more than Rs. 1000.
+# On orders of more than Rs. 100 for key-based toys, a discount of 5% is given,
+# and a discount of 10% is given on orders for electrical charging based toys of value more than Rs. 500.
+# Assume that the numeric codes 1,2 and 3 are used for battery based toys, key-based toys, and electrical charging based toys respectively.
+# Write a program that reads the product code and the order amount and prints out the net amount that the customer is required to pay after the discount.
+
