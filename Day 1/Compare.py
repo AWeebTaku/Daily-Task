8d95fbeab0e150e4532e278a5cbc54678142c4f2
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 17:16:42 +0530

    Update Compare.py

--- Day 1/Compare.py
+++ Day 1/Compare.py
@@ -1,9 +1,9 @@
 #2. Declare two variables and print that which variable is largest using ternary operators
 
-num1 = int(input("Enter a number:"))
-num2 = int(input("Enter a number:"))
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
 
 if num1 < num2:
-    print(num2 ,"is Greater than", num1)
+    print(num2 ,"is Greater than",num1)
 else:
-    print(num1 ,"is Greater than", num2)
+    print(num1 ,"is Greater than",num2)
