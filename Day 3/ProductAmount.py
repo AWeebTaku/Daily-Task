619606b2ce86607a204610459626650be264bacc
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 18:01:42 +0530

    Update ProductAmount.py

--- Day 3/ProductAmount.py
+++ Day 3/ProductAmount.py
@@ -5,3 +5,33 @@
 # Assume that the numeric codes 1,2 and 3 are used for battery based toys, key-based toys, and electrical charging based toys respectively.
 # Write a program that reads the product code and the order amount and prints out the net amount that the customer is required to pay after the discount.
 
+ProductCode = int(input("Enter a Product Code: "))
+
+if ProductCode == 1:
+	BatteryPrice = int(input("Enter Amount: "))
+	if BatteryPrice>=1000:
+		Discount = BatteryPrice*10/100
+		NetPrice = BatteryPrice - Discount
+		print("Your Net Amount is:",NetPrice)
+	else:
+		print("Your Net Amount is:",BatteryPrice)
+
+
+if ProductCode == 2:
+	KeyPrice = int(input("Enter Amount: "))
+	if KeyPrice>=100:
+		Discount = KeyPrice*5/100
+		NetPrice = KeyPrice - Discount
+		print("Your Net Amount is:",NetPrice)
+	else:
+		print("Your Net Amount is:",KeyPrice)
+
+
+if ProductCode == 3:
+	ElectricalPrice = int(input("Enter Amount: "))
+	if ElectricalPrice>=500:
+		Discount = KeyPrice*10/100
+		NetPrice = ElectricalPrice - Discount
+		print("Your Net Amount is:",NetPrice)
+	else:
+		print("Your Net Amount is:",ElectricalPrice)
