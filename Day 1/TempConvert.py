e1f5412170dd4e03d789c3190e376acbd715b708
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 17:16:47 +0530

    Update TempConvert.py

--- Day 1/TempConvert.py
+++ Day 1/TempConvert.py
@@ -1,5 +1,5 @@
 #3. Python program to convert the temperature in degree centigrade to Fahrenheit
 
-tempC = int(input("Enter Temperature(°C):"))
+tempC = int(input("Enter Temperature(°C): "))
 tempF = (tempC * 9/5) + 32
 print("Temperature(°F):",tempF)
