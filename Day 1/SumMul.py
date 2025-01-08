cf70b490112143e3190ca378852b26b5112fd213
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-08 16:13:07 +0530

    Update SumMul.py

--- Day 1/SumMul.py
+++ Day 1/SumMul.py
@@ -1,6 +1,10 @@
+#1. Calculate the multiplication and sum of two numbers
+
 num1 = int(input("Enter a number:"))
 num2 = int(input("Enter a number:"))
+
 sum = num1 + num2
 pro = num1 * num2
+
 print("Sum of Two Numbers = ", sum)
 print("Multiplication product of Two Numbers = ", pro)
