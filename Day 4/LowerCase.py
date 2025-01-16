3b6f05071297eaaf259d419163dad70910a93f5f
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-16 19:05:36 +0530

    Update LowerCase.py

--- Day 4/LowerCase.py
+++ Day 4/LowerCase.py
@@ -1,4 +1,4 @@
 #4. Accept a name from the user and display that in lower case using lower() function.
 
 user_name = input("Enter your name in Upper Case: ")
-print("Your Name is Lower Case:",user_name.lower())
+print("Your Name in Lower Case:",user_name.lower())
