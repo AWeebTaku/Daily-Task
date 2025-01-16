a3f40e7df71875ac8ac3031a149a358b616a5b08
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-16 19:04:33 +0530

    Create MaxMin.py

---
+++ Day 4/MaxMin.py
@@ -0,0 +1,8 @@
+#3. Using max() and min() functions display the maximum and minimum of 5 random numbers.
+
+numbers = input("Enter 5 Random Numbers(Sepearted by Spaces): ").split()
+
+print("Random Numbers:",numbers)
+
+print("Maximum Value:",max(numbers))
+print("Minimum Value:",min(numbers))
