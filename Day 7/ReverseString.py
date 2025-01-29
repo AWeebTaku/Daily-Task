4599ff2805020c962717342b563349362a8669a1
Aditya Chaurasiya <146383663+AWeebTaku@users.noreply.github.com>
2025-01-29 16:34:34 +0530

    Create ReverseString.py

---
+++ Day 7/ReverseString.py
@@ -0,0 +1,7 @@
+#3. Write a Python program to reverse words in a string String = “Deeptech Python Training” 
+
+str = "Deeptech Python Training"
+print("Original String:",str)
+
+reverse = str[::-1]
+print("Reversed String:",reverse)
